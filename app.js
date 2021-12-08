40b0d55b81cbcd5961416ed4c4087e38e20cefc4
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2021-12-08 16:12:31 -0600

    adding javaScript data adjustments

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 import { characters } from './data.js';
 console.log(characters);
+/*Template initializing */
+const template = document.getElementById('template');
 
+//loop the array 
+for (let character of characters) {
+    console.log(characters);
+    const copy = template.content.cloneNode(true);
+    const header = copy.querySelector('h2');
+    const image = copy.querySelector('img');
+    const button = copy.querySelector('button');
+    console.log(header, image, button);
+    header.textContent = character.name;
+    image.src = character.image;
+    console.log(copy);
+
+
+
+}
+// for each character
+//make a copy of the template
+
+/* Audio intializing */
 const audio = document.getElementById('audio');
-
+/* character sound connecting */
 const spongebobSound = document.getElementById('spongebob-sound');
 spongebobSound.addEventListener('click', () => {
     audio.src = 'assets/sounds/spongebob.mp3';
@@ -37,11 +58,13 @@ christmasRed.addEventListener('click', () => {
     audio.src = 'assets/sounds/christmas.mp3';
     audio.play();
 });
+
 const christmas2Sound = document.getElementById('christmas2-sound');
 christmas2Sound.addEventListener('click', () => {
     audio.src = 'assets/sounds/christmas2.mp3';
     audio.play();
 });
+
 const christmas3Sound = document.getElementById('christmas3-sound');
 christmas3Sound.addEventListener('click', () => {
     audio.src = 'assets/sounds/christmas3.mp3';

cdb0ae59fd54bb82d5141c8206f7788a2e40b583
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2021-12-08 14:06:51 -0600

    Added audio and color theme

---
+++ app.js
@@ -0,0 +1,19 @@
+const audio = document.getElementById('audio');
+
+const spongebobSound = document.getElementById('spongebob-sound');
+spongebobSound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/spongebob.mp3';
+    audio.play();
+});
+
+const spriteSound = document.getElementById('among-sound');
+spriteSound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/among.mp3';
+    audio.play();
+});
+
+const timmySound = document.getElementById('timmy-sound');
+timmySound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/timmy.mp3';
+    audio.play();
+});
